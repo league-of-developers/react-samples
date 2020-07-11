3dddd6382f1df4a82617303ac531610f25322a70
Juliano Lima <juliano.lima@nubank.com.br>
2020-07-11 18:49:49 -0300

    Adds a few rows on counter

--- src/counter/index.jsx
+++ src/counter/index.jsx
@@ -1,31 +1,32 @@
 import React, { Component } from "react";
-import { Button } from '../components/Button';
+import { Button } from "../components/Button";
+import { Container } from "../components/Container";
+import { Row } from "../components/Row";
 
 class Counter extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {value: 0};
-    }
+  constructor(props) {
+    super(props);
+    this.state = { value: 0 };
+  }
 
-    incrementValue = (e) => {
-        this.setState({value: this.state.value + 1});
-    }
+  incrementValue = (e) => {
+    this.setState({ value: this.state.value + 1 });
+  };
 
-    decrementValue = (e) => {
-        this.setState({value: this.state.value - 1});
-    }
-  
-    render() {
+  decrementValue = (e) => {
+    this.setState({ value: this.state.value - 1 });
+  };
+
+  render() {
     return (
-      <div>
-        <p>{this.state.value}</p>
-        <Button onClick={this.incrementValue}
-                label="+"/>
-        <Button
-          onClick={this.decrementValue}
-          label="-"
-        />
-      </div>
+      <Container>
+        
+        <Row>Valor atual: {this.state.value}</Row>
+        <Row>
+          <Button onClick={this.incrementValue} label="+" color="primary" />
+          <Button onClick={this.decrementValue} label="-" color="danger"/>
+        </Row>
+      </Container>
     );
   }
 }
