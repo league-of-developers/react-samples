418cf7d7d9cbd392bbbcd843bb088e79602058c3
Juliano Lima <juliano.lima@nubank.com.br>
2020-07-11 18:49:05 -0300

    Create row component

---
+++ src/components/Row.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Row as BootstrapRow } from "reactstrap";
+
+const Row = ({children}) => (
+    <BootstrapRow>
+        {children}
+    </BootstrapRow>
+)
+
+export { Row }
