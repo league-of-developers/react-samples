5bfa6cb6243822dfd193c91c9c8757683739d0b7
Juliano Lima <juliano.lima@nubank.com.br>
2020-09-20 18:28:47 -0300

    Atualiza counter

--- src/counter/index.jsx
+++ src/counter/index.jsx
@@ -1,42 +1,30 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import Button from '../components/Button';
-import Container from '../components/Container';
-import Row from '../components/Row';
 
-class Counter extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { value: 0 };
-  }
-
-  incrementValue = () => {
-    const { value } = this.state;
-    this.setState({ value: value + 1 });
-  };
-
-  decrementValue = () => {
-    const { value } = this.state;
-    this.setState({ value: value - 1 });
-  };
-
-  render() {
-    const { value } = this.state;
-    return (
-      <Container>
-        <Row>
-          <p>
-            Valor atual:
-            {' '}
-            {value}
-          </p>
-        </Row>
-        <Row>
-          <Button onClick={this.incrementValue} label="+" color="primary" />
-          <Button onClick={this.decrementValue} label="-" color="danger" />
-        </Row>
-      </Container>
-    );
-  }
-}
+const Counter = () => {
+  const [count, setCount] = useState(0);
+  return (
+    <div>
+      <p>
+        {' '}
+        {count}
+        {' '}
+      </p>
+      <Button
+        onClick={() => {
+          setCount(count + 1);
+        }}
+        label="+"
+      />
+      <Button
+        onClick={() => {
+          setCount(count - 1);
+        }}
+        label="-"
+        color="danger"
+      />
+    </div>
+  );
+};
 
 export default Counter;
